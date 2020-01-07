ba468391a33546533f182f3ecb2b13abd5278c2e
Abdullah Al Numan <ab>
2020-01-06 21:42:31 -0800

    done

---
+++ Pods/Target Support Files/Pods-kingFisherUrl/Pods-kingFisherUrl-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_kingFisherUrl : NSObject
+@end
+@implementation PodsDummy_Pods_kingFisherUrl
+@end
--- kingFisherUrl/ViewController.swift
+++ kingFisherUrl/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var test: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        test.kf.indicatorType = .activity
+        test.kf.setImage(with: URL(string: "https://www.joc.com/sites/default/files/field_feature_image/ILWU-NRF-NAM-billions-dollars-shutterstock_142729792-600.jpg"),placeholder: nil ,options: [.transition(.fade(0.7))],progressBlock: nil)
     }
 
 
